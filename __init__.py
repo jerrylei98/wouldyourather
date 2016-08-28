9c3a3948bcf83fa0877560d29ddd8cab2bb2d4f4
jerrylei98 <jerrylei98@gmail.com>
2016-08-28 00:36:59 -0400

    can upload prof pics/profile page

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, session, request, redirect
+from utils import *
+import os
 
 app = Flask(__name__)
 app.secret_key = 'thequickbrownfoxjumpsoverthelazydog'
@@ -6,12 +8,41 @@ app.secret_key = 'thequickbrownfoxjumpsoverthelazydog'
 @app.route("/", methods=["GET","POST"])
 def index():
     if request.method == "GET":
-        return render_template("index.html", username = session.get('user'))
+        return render_template("index.html", username = session.get('username'))
     elif request.method == "POST":
-        button = request.form('button')
-        return render_template("index.html", username = session.get('user'))
+        button = request.form['button']
+        if button == "Sign In":
+            uname = request.form.get('in_username')
+            pword = request.form.get('in_password')
+            if check_user(uname,pword):
+                session['username'] = uname
+            return render_template("index.html", username = session.get('username'))
+        if button == "Sign Up":
+            uname = request.form.get('up_username')
+            pword = request.form.get('up_password')
+            if create_user(uname, pword):
+                session['username'] = uname
+            return render_template("index.html", username = session.get('username'))
+        return render_template("index.html", username = session.get('username'))
 
-@app.route("/logout")
+@app.route("/profile")
+@app.route("/profile/<uname>", methods=["GET","POST"])
+def profile(uname = ""):
+    if os.path.isfile('./static/img/prof_pics/' + session.get('username') + '.png'):
+        pic = "../static/img/prof_pics/" + session.get('username') + '.png'
+    else:
+        pic = "../static/img/user.png"
+    if request.method == "GET":
+        return render_template("profile.html", username = session.get('username'), prof_pic = pic)
+    elif request.method == "POST":
+        file = request.files['file']
+        if file and ".png" in file.filename:
+            file.save(os.getcwd() + '/static/img/prof_pics/' + session.get('username') + '.png')
+            pic = "../static/img/prof_pics/" + session.get('username') + '.png'
+            return render_template("profile.html", username = session.get('username'), prof_pic = pic)
+        return render_template("profile.html", username = session.get('username'), prof_pic = pic)
+
+@app.route("/logout") ##clears session
 def logout():
     session['username'] = None
     return redirect("/")
